e9feb46412f8c016c4050679b930262fbefdd795
Adam Menz <adam.menz@rackspace.com>
2011-06-15 10:51:10 -0500

    Contact rackspace support fully implemented

--- src/com/rackspacecloud/android/ContactActivity.java
+++ src/com/rackspacecloud/android/ContactActivity.java
@@ -4,16 +4,59 @@ import java.util.ArrayList;
 
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class ContactActivity extends Activity{
 	
 	ArrayList<String[]> contacts;
+	private final String USNUMBER = "1-877-934-0407";
+	private final String UKNUMBER = "0800-083-3012";
+	private final String twitterAddress = "twitter.com/rackspace";
 	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-      //  setContentView(R.layout.);
-        //registerForContextMenu(getListView());
+        setContentView(R.layout.contactrackspace);
+        setUpButtons();
     }
+	
+	private void setUpButtons(){
+		
+		Button usButton = (Button)findViewById(R.id.contact_us_button);
+		usButton.setText(USNUMBER);
+		usButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v){
+				String phone = "tel:" + USNUMBER;
+				Intent i = new Intent(Intent.ACTION_DIAL, Uri.parse(phone));
+				startActivity(i);
+			}
+		});
+		
+		Button ukButton = (Button)findViewById(R.id.contact_uk_button);
+		ukButton.setText(UKNUMBER);
+		ukButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v){
+				String phone = "tel:" + UKNUMBER;
+				Intent i = new Intent(Intent.ACTION_DIAL, Uri.parse(phone));
+				startActivity(i);
+			}
+		});
+		
+		Button twitterButton = (Button)findViewById(R.id.contact_twitter_button);
+		twitterButton.setText(twitterAddress);
+		twitterButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v){
+				Log.d("info", "http://" + twitterAddress);
+				Intent browse = new Intent(Intent.ACTION_VIEW , Uri.parse("http://mobile.twitter.com/rackspace"));
+			    startActivity(browse);
+
+			}
+		});
+	
+	}
 
 }
